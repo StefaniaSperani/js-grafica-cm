cadf7157bdc34a422b1625256bd923a9d6994453
StefaniaSperani <thedarkcry@hotmail.it>
2022-10-17 17:18:35 +0200

    draw cell and grid functions

--- js/script.js
+++ js/script.js
@@ -12,3 +12,68 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe; */
 
+
+/* 
+
+<div class="square">
+    <span>1</span>
+</div>
+
+*/
+
+
+
+
+// tengo lo scope globale PULITO, quindi prendo solo il
+// button Play, che è l'unica cosa che mi serve
+const playBtn = document.getElementById('play');
+
+//creo la funzione che mi permette di ottenere 
+//l'azione nel blocco al blick del bottone
+function play(){
+// console.log('Inizia!');
+
+    //2.ora creo una variabile che mi indica il numero di celle 
+    //da inserire dentro l griglia di gioco
+    const numCell = 100;
+
+    //2.creo la funzione che crea la cella
+    function drawCell(numCell){
+        //creo il div dove creerò il quadrato
+        const cell = document.createElement('div');
+        //e gli aggiungo la classe che gli darà lo stile dal css
+        cell.className = 'square';
+        //devo inserire il numero nell'html, e quindi
+        cell.innerHTML = `
+            <span>${numCell}</span>
+        `;
+        //cell deve avere un ritorno? Si, deve 
+        //tornare la cell
+        return cell;
+    }
+    //creo la funzione che mi andrà a creare la GRIGLIA del gioco
+    function drawGrid(){
+        //creo la variabile che mi servirà per appenderci cio che creo
+        const playground = document.getElementById('playground');
+        //creo il div dove andrò ad appendere la griglia
+        const grid = document.createElement('div');
+        //e gli aggiungo la classe che gli darà lo stile dal css
+        grid.className = 'grid';
+
+        for(let i = 1; i <= 100; i++){
+            //2.ora qua metto la variabile cell
+            //che dovrà girare fin quando  non creerà tutti i div
+            const cell = drawCell(i);
+            //ora appendo la cell alla griglia
+            grid.appendChild(cell);
+        }
+        //ora appendo la griglia al div principale, playground
+        playground.appendChild(grid);
+
+    }
+
+}
+//ora che ho la funzione pronta, la attacco
+//al bottone play, e al click eseguirà quello che gli ho detto
+//di fare nella funzione
+playBtn.addEventListener('click', play);
